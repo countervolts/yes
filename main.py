cf1095c389fcf980498c5744f909bba646b3473c
countervolts <93222141+countervolts@users.noreply.github.com>
2022-01-28 18:31:20 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_button_pressed_b():
+    basic.show_number(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_a():
+    basic.show_number(2)
+input.on_button_pressed(Button.A, on_button_pressed_a)
---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    basic.showNumber(1)
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.showNumber(2)
+})
